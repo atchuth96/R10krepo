e31374df93aaef3ac82804acb88c84355b784dad
atchuth96 <k.atchuthreddy1996@gmail.com>
2020-08-28 15:26:16 +0530

    Add files via upload

---
+++ update_aces.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+appname=sys.argv[1]
+buildNum=sys.argv[2]
+matched=False
+updated=False
+print("Updating "+appname+" for build "+buildNum)
+contentline=open("puppetfile.txt","r")
+content=contentline.readlines()
+print(content)
+for i in range(0,len(content)):
+    if (i+2) < len(content):
+        print(content[i])
+        if appname in content[i]:
+            matched=True
+            if matched and not updated:
+                k=i+2
+                newcontent=":ref => '1.1."+buildNum+"'"
+                print("Replacing "+content[k]+" with "+newcontent)
+                content[k]=newcontent
+                updated=True
+f = open("puppetfile.txt", "w")
+new_file_contents = "". join(content)
+f.write(new_file_contents)
+f.close()
+                
+
+

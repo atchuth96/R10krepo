5dbbdc32cb0a0efd62b8e1cc9de0c3717a50ae8b
atchuth96 <k.atchuthreddy1996@gmail.com>
2020-09-07 13:40:53 +0530

    Update update.py

--- update.py
+++ update.py
@@ -3,8 +3,8 @@ import base64
 import git
 from git import Repo
 
-r=Repo()
-tags=r.tags
+repo=Repo()
+tags=repo.tags
 lasttag=tags[-1]
 tag='1.1.3'
 new_tag = repo.create_tag(tag, message='Automatic tag "{0}"'.format(tag)) 

b427aed673316a6391e2f794482820e83d1d3f74
atchuth96 <k.atchuthreddy1996@gmail.com>
2020-09-09 19:26:55 +0530

    Commit

--- update.py
+++ update.py
@@ -1,13 +1,17 @@
 import requests
 import base64
-import git
-from git import Repo
+import os
+import subprocess
 
-repo=Repo()
+
+stream = os.popen('echo Returned output')
+output = stream.read()
+print(output)
+"""repo=Repo()
 tags=repo.tags
 lasttag=tags[-1]
 tag='1.1.3'
 new_tag = repo.create_tag(tag, message='Automatic tag "{0}"'.format(tag)) 
-repo.remotes.origin.push(new_tag)
+repo.remotes.origin.push(new_tag)"""
 
 
